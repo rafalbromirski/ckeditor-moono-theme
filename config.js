238912af7ea093d3b4d34b35a657131610c37950
paranoida <rafal.bromirski@gmail.com>
2012-10-04 20:53:37 +0200

    resizer + toolbar icons

--- config.js
+++ config.js
@@ -5,6 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Just put the name of your custom skin here.
-	config.skin = 'moono-light';
+    config.skin = 'moono-light';
+    //config.skin = 'moono-dark';
 };
 
